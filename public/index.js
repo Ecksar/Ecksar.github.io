85620a9c429feaaf553d469f99746c0b1f3d4efc
Glitch (hello-express-old) <none>
2024-05-17 13:50:44 +0000

    📍🌗 Checkpoint ./public/index.js:65092273/344 ./views/index.html:65092273/2802

--- public/index.js
+++ public/index.js
@@ -1,5 +1,9 @@
 console.log("running on home");
 function submitLink(link){
-  let inputLink = document.getElementById("submitLink").value;
-  alert("OUCH! Link: " + inputLink);
+  let inputLinkVal = document.getElementById("link").value;
+  let finishedLink = document.getElementById("finishedLink");
+  
+  let final = inputLinkVal;
+  finishedLink.innerHTML = final;
+  finishedLink.href = final;
 }
